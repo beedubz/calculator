abdd5cc18bad3b9af94e7565075551ffd7e61b5a
Brendan <167249264+beedubz@users.noreply.github.com>
2025-03-24 13:14:38 -0600

    Added number and operator buttons, display, and made operator buttons clear display

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+/*
 let a = null;
 let b = null;
 let operator = null;
@@ -19,4 +20,26 @@ const operate = function(a, b, operator) {
 
 if (typeof result === "number") {
     let a = result;}
+*/
+document.addEventListener('DOMContentLoaded', function() {
+    const display = document.getElementById('display');
+    const numbuttons = document.querySelectorAll('.number-button');
+        numbuttons.forEach(numbutton => {
+          numbutton.addEventListener('click', function() {
+            display.textContent += this.textContent;
+          });
+        });
+    
 
+    const operators = document.querySelectorAll('.operator');
+    console.log(operators);
+    console.log('Operators found:', operators);
+    const allButtons = document.querySelectorAll('button');
+    console.log('All buttons:', allButtons);
+    operators.forEach(operator => {
+                operator.addEventListener('click', function() {
+                    display.textContent = '';
+                console.log('Operator button clicked!');
+                });
+            });
+        });
